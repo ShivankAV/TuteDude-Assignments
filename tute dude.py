c9a87ec1ad943ef614eb1d6761efee8ee70c1e5d
ShivankAV <127657671+ShivankAV@users.noreply.github.com>
2025-10-05 18:01:32 +0530

    Delete tute dude.py

--- tute dude.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter the first number:"))
-b=int(input("Enter the second number:"))
-print("\n")
-print("Addition: ",a+b)
-print("Substraction: ",a-b)
-print("Multiplication: ",a*b)
-print("Division: ",a/b)
